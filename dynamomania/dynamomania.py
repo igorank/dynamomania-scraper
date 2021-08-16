f8e05c784e986039890f77a16921dac0c2a75166
Igor Anikin <gotogo1995@gmail.com>
2021-08-16 23:07:35 +0300

    Constant name url has been renamed to URL

--- dynamomania/dynamomania.py
+++ dynamomania/dynamomania.py
@@ -1,7 +1,7 @@
-import requests
 import time
 import webbrowser
 import re
+import requests
 
 #from win10toast import ToastNotifier
 #from plyer import notification
@@ -10,9 +10,9 @@ from win10toast_click import ToastNotifier
 
 toaster = ToastNotifier()
 
-url = 'https://www.dynamomania.com'
+URL = 'https://www.dynamomania.com'
 
-html_text = requests.get(url).text
+html_text = requests.get(URL).text
 soup = BeautifulSoup(html_text, 'html.parser')
 contentTable = soup.find('div', { "class" : "news"})
 
@@ -20,7 +20,7 @@ link = contentTable.find('a', attrs={'href' : re.compile("^/news/")})
 link_title = link.find('div', { "class" : "text-main bold"})
 
 def open_url():
-    webbrowser.open_new(url + link2.get('href'))
+    webbrowser.open_new(URL + link2.get('href'))
 
 def get_nextlink(lnk):
     lnk = lnk.find_next('a')
@@ -35,31 +35,31 @@ try:
 except AttributeError:
     link = get_nextlink(link)
     link_title = get_nexttitle(link_title,link)
-        
+
 while True:
     # reparse content
-    html_text = requests.get(url).text
+    html_text = requests.get(URL).text
     soup = BeautifulSoup(html_text, 'html.parser')
     contentTable = soup.find('div', { "class" : "news"})
-    
+
     link2 = contentTable.find('a', attrs={'href' : re.compile("^/news/")})
     link2_title = link2.find('div', { "class" : "text-main bold"})
-    
+
     try:
         link2_title.get_text()
     except AttributeError:
         link2 = get_nextlink(link2)
         link2_title = get_nexttitle(link2_title,link2)
-   
+
     if link2_title.get_text() != link_title.get_text():
         toaster.show_toast(
         "Dynamomania.com",
-        link2_title.get_text(), 
+        link2_title.get_text(),
         icon_path='icon.ico',
-        duration=60, 
-        threaded=True,  
-        callback_on_click=open_url 
+        duration=60,
+        threaded=True,
+        callback_on_click=open_url
         )
         link_title = link2_title
-            
+
     time.sleep(60)
